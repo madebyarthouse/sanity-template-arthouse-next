f68c32c85a597b86ca17d6e3e97736038c88a622
Christian Cito <christiancito@hey.com>
2021-10-19 17:41:58 +0200

    Change bundle size

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,15 @@
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => <div>Change in Bundle size 1</div>;
+const Home: NextPage = () => (
+  <div>
+    Change in Bundle size 123455 Lorem ipsum dolor sit amet consectetur
+    adipisicing elit. Provident molestias vel dignissimos voluptatem, culpa
+    voluptate soluta magnam consequatur veritatis! Qui earum veniam id
+    repellendus nostrum itaque saepe nesciunt optio provident? Lorem ipsum dolor
+    sit amet, consectetur adipisicing elit. Veniam magnam labore assumenda,
+    eligendi, quisquam suscipit nesciunt voluptates maiores, quae in sit cum
+    rerum aspernatur quia amet soluta deserunt. Asperiores, amet!
+  </div>
+);
 
 export default Home;
