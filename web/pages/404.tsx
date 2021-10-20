7404a35d4efe50fb02f01565471a2fadb691d7f7
Christian Cito <christiancito@hey.com>
2021-10-20 16:45:51 +0200

    Add custom 404 + next-seo

---
+++ web/pages/404.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+
+const Page404: NextPage = () => (
+  <div>
+    <h1>404</h1>
+    <p>Page not found</p>
+  </div>
+);
+
+export default Page404;
--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,8 +1,28 @@
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
 import '@styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo
+        title="Arthouse Next.js Starter"
+        titleTemplate="%s / Arthouse Next.js Starter"
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://www.url.ie/',
+          site_name: 'Arthouse Next.js Starter',
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image',
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
